b5a19571442a1777a97d0821226e9ca66671cfb6
john0999 <moralesgomezjohnny@gmail.com>
2024-10-29 08:34:32 -0600

    Fusion with branch Josafat

--- app/src/main/java/com/example/redautenapp/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/redautenapp/ui/screens/home/HomeScreen.kt
@@ -1,5 +1,13 @@
 package com.example.redautenapp.ui.screens.home
-Josafat HomeScreen.kt
+///Josafat HomeScreen.kt
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun HomeScreen(auth: FirebaseAuth, onLogout: () -> Unit) {
