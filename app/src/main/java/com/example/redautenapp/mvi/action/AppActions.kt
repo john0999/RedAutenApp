14d2a771fdbacf77cdc8cca38b1ad2c77424085d
josaguapo7 <josafatrujillo604@gmail.com>
2024-10-28 20:24:54 -0600

    subiendo arcchivos de HomeScreen.kt

--- app/src/main/java/com/example/redautenapp/mvi/action/AppActions.kt
+++ app/src/main/java/com/example/redautenapp/mvi/action/AppActions.kt
@@ -18,40 +18,4 @@ fun AppActions(auth: FirebaseAuth) {
     } else {
         LoginScreen(auth) { isLoggedIn = true }
     }
-
-    Josafat HomeScreen.kt
-
-            @Composable
-            fun HomeScreen(auth: FirebaseAuth, onLogout: () -> Unit) {
-                val currentUser = auth.currentUser
-                val userName = currentUser?.displayName ?: "Usuario"
-                val userEmail = currentUser?.email ?: "Correo no disponible"
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text("Bienvenido a la Aplicación", style = MaterialTheme.typography.headlineMedium)
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    Text("Nombre de usuario: $userName")
-                    Text("Correo: $userEmail")
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    Button(onClick = {
-                        auth.signOut()
-                        onLogout()
-                    }) {
-                        Text("Cerrar sesión")
-                    }
-                }
-            }
 }
-
-
-
--- app/src/main/java/com/example/redautenapp/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/redautenapp/ui/screens/home/HomeScreen.kt
@@ -1,1 +1,33 @@
 package com.example.redautenapp.ui.screens.home
+Josafat HomeScreen.kt
+
+@Composable
+fun HomeScreen(auth: FirebaseAuth, onLogout: () -> Unit) {
+    val currentUser = auth.currentUser
+    val userName = currentUser?.displayName ?: "Usuario"
+    val userEmail = currentUser?.email ?: "Correo no disponible"
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Bienvenido a la Aplicación", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("Nombre de usuario: $userName")
+        Text("Correo: $userEmail")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            auth.signOut()
+            onLogout()
+        }) {
+            Text("Cerrar sesión")
+        }
+    }
+}
