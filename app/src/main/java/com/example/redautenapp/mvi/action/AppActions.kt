61b6eceee71783fd7ca84d3b9e682be770607a7a
Luis Hernandez <soyluishs@gmail.com>
2024-10-29 13:11:12 -0600

    Agregando funciones handle

--- app/src/main/java/com/example/redautenapp/mvi/action/AppActions.kt
+++ app/src/main/java/com/example/redautenapp/mvi/action/AppActions.kt
@@ -19,3 +19,48 @@ fun AppActions(auth: FirebaseAuth) {
         LoginScreen(auth) { isLoggedIn = true }
     }
 }
+
+// Artemio
+
+fun handleLogin(auth: FirebaseAuth, email: String, password: String, context: Context, onSuccess: () -> Unit) {
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                saveUserToDatabase(auth.currentUser)
+                Toast.makeText(context, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+                onSuccess()
+            } else {
+                val errorMessage = task.exception?.message ?: "Error desconocido"
+                Toast.makeText(context, "Error: $errorMessage", Toast.LENGTH_SHORT).show()
+            }
+        }
+}
+
+fun handleRegistration(auth: FirebaseAuth, email: String, password: String, context: Context, onSuccess: () -> Unit) {
+    auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                saveUserToDatabase(auth.currentUser)
+                Toast.makeText(context, "Registro exitoso", Toast.LENGTH_SHORT).show()
+                onSuccess()
+            } else {
+                val errorMessage = task.exception?.message ?: "Error desconocido"
+                Toast.makeText(context, "Error: $errorMessage", Toast.LENGTH_SHORT).show()
+            }
+        }
+}
+
+fun handleTwitterLogin(auth: FirebaseAuth, context: Context, onSuccess: () -> Unit) {
+    val provider = OAuthProvider.newBuilder("twitter.com")
+    auth.startActivityForSignInWithProvider(context as ComponentActivity, provider.build())
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                saveUserToDatabase(auth.currentUser)
+                Toast.makeText(context, "Inicio de sesión con Twitter exitoso", Toast.LENGTH_SHORT).show()
+                onSuccess()
+            } else {
+                val errorMessage = task.exception?.message ?: "Error desconocido"
+                Toast.makeText(context, "Error: $errorMessage", Toast.LENGTH_SHORT).show()
+            }
+        }
+}
